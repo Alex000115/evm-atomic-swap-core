7d0ad8cfffdaca13db76a2124ea61a3819f50cc5
Alex <160778418+Alex000115@users.noreply.github.com>
2026-01-19 14:05:46 +0600

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,22 @@
+/**
+ * Immutable constants and ABI definitions.
+ */
+
+const ERC20_ABI = [
+    "function balanceOf(address owner) view returns (uint256)",
+    "function decimals() view returns (uint8)",
+    "function symbol() view returns (string)",
+    "function approve(address spender, uint256 amount) returns (bool)",
+    "function allowance(address owner, address spender) view returns (uint256)",
+    "event Transfer(address indexed from, address indexed to, uint256 amount)"
+];
+
+const ROUTER_ABI = [
+    "function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)",
+    "function getAmountsOut(uint amountIn, address[] calldata path) external view returns (uint[] memory amounts)"
+];
+
+module.exports = {
+    ERC20_ABI,
+    ROUTER_ABI
+};
